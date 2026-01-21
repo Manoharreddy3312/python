c11a3c9596604e4ef0495e5cf34c2aa3be4d4bd5
Manoharreddy <mosarlamanoharreddy2@gmail.com>
2026-01-21 09:50:50 +0530

    your message

---
+++ Python/AdvancedConcept/filehandling.py
@@ -0,0 +1,28 @@
+# FILE HANDLING
+
+'''
+    It is a phenomena of writing and reading inside a file.
+    It is an concept to handle file by modifying it with respective function.
+    
+    modes of file handling :
+    ``````````````````
+    1) write
+    2) read
+    3) append
+'''
+
+'''
+    syntax :
+    
+    with open('loc',mode) as var :
+        S.B
+'''
+        
+    #write mode function :
+'''
+    1) var.write(data)----------> used for single line data
+    2) var.writelines(data)-----> used for multiple line of data
+'''
+
+# Read mode function
+
